28499d13071b80a9ea8313a7979ec003587b1c65
b2un0 <branko.wilhelm@gmail.com>
2016-04-19 18:50:00 +0200

    - fix network bug

--- resources/lib/hyperion.py
+++ resources/lib/hyperion.py
@@ -1,5 +1,6 @@
 import json
 import socket
+import time
 import xbmc
 
 from resources.lib import webcolors
@@ -92,7 +93,7 @@ class Remote:
 
             s.sendall(data)
 
-            data = s.recv(4096)
+            data = self._recv(s)
 
             s.close()
 
@@ -100,3 +101,38 @@ class Remote:
             raise Exception(e)
 
         return json.loads(data)
+
+    def _recv(self, s, timeout=0.1):
+        # make socket non blocking
+        s.setblocking(0)
+
+        # total data partwise in an array
+        total_data = []
+        data = ''
+
+        # beginning time
+        begin = time.time()
+        while 1:
+            # if you got some data, then break after timeout
+            if total_data and time.time() - begin > timeout:
+                break
+
+            # if you got no data at all, wait a little longer, twice the timeout
+            elif time.time() - begin > timeout * 2:
+                break
+
+            # recv something
+            try:
+                data = s.recv(8192)
+                if data:
+                    total_data.append(data)
+                    # change the beginning time for measurement
+                    begin = time.time()
+                else:
+                    # sleep for sometime to indicate a gap
+                    time.sleep(0.1)
+            except:
+                pass
+
+        # join all parts to make final string
+        return ''.join(total_data)
